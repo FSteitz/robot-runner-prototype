49f4bc36a82b7993f04e233753d450c6b56afc10
Florian Steitz <florian@fsteitz.com>
2016-04-09 16:58:18 +0100

    Added GameController. Each class that would extend MonoBehavior should extend this class instead as it provides quick access to the core components of a game, e.g. the GameContext instance.

--- robot-runner-mobile-game/Assets/Scripts/Common/Controller/GameController.cs
+++ robot-runner-mobile-game/Assets/Scripts/Common/Controller/GameController.cs
@@ -1,9 +1,23 @@
 ﻿using System;
 
-namespace AssemblyCSharp {
-	public class GameController {
-		public GameController() {
-		}
+using UnityEngine;
+
+using Vaneftec.Game.Common.Context;
+using Vaneftec.Game.Common.Level;
+using Vaneftec.Game.Common.Score;
+
+// Copyright (c) Florian Steitz
+namespace Vaneftec.Game.Common.Controller {
+
+	/// <summary>
+	/// TODO
+	/// </summary>
+	public abstract class GameController : MonoBehaviour {
+		
+		protected static GameContext gameContext = GameContext.get();
+
+		protected LevelManager levelManager = gameContext.LevelManager;
+		protected ScoreManager scoreManager = gameContext.ScoreManager;
 	}
 }
 
