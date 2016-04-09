b00eb15d9efe82a04cf531199d6e469b757866cb
Florian Steitz <florian@fsteitz.com>
2016-04-09 16:55:39 +0100

    Implemented GameContext. The GameContext is a singleton that encapsulates the core functionalities of a game. It is the single point of entry for each of the core components such as the ScoreManager and LevelManager.

--- robot-runner-mobile-game/Assets/Scripts/Common/Context/GameContext.cs
+++ robot-runner-mobile-game/Assets/Scripts/Common/Context/GameContext.cs
@@ -1,15 +1,56 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
 
-public class GameContext {
+using Vaneftec.Game.Common.Singleton;
+using Vaneftec.Game.Common.Level;
+using Vaneftec.Game.Common.Score;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+// Copyright (c) Florian Steitz
+namespace Vaneftec.Game.Common.Context {
+
+	/// <summary>
+	/// TODO
+	/// </summary>
+	public class GameContext {
+
+		public LevelManager LevelManager { get; set; }
+		public ScoreManager ScoreManager { get; set; }
+
+		private static GameContext instance;
+
+		/// <summary>
+		/// TODO
+		/// </summary>
+		public GameContext() {
+			LevelManager = new LevelManager();
+			ScoreManager = new ScoreManager();
+		}
+
+		/// <summary>
+		/// TODO
+		/// </summary>
+		public void StartLevel() {
+			if (LevelManager.CountLevels() == 0) {
+				PlayableLevel level = new PlayableLevel();
+				level.Name = "firstLevel";
+				level.CurrentScore = ScoreManager.DEFAULT_SCORE;
+				level.LastScore = ScoreManager.DEFAULT_SCORE;
+				level.HighestScore = ScoreManager.DEFAULT_SCORE;
+
+				LevelManager.AddLevel(level);
+				ScoreManager.CurrentLevel = level;
+			}
+		}
+
+		/// <summary>
+		/// TODO
+		/// </summary>
+		public static GameContext get() {
+			if (instance == null) {
+				instance = new GameContext();
+			}
+
+			return instance;
+		}
 	}
 }
