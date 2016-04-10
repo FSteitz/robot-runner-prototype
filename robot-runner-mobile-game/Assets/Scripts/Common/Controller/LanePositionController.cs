8eec00781a7deb3bca12aeb560c4ac31e2dbd8c1
Florian Steitz <florian@fsteitz.com>
2016-04-10 15:12:02 +0100

    Added .meta files missing from previous commit.

---
+++ robot-runner-mobile-game/Assets/Scripts/Common/Controller/LanePositionController.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AssemblyCSharp {
+	public class LanePositionController {
+		public LanePositionController() {
+		}
+	}
+}
+
---
+++ robot-runner-mobile-game/Assets/Scripts/Common/Controller/RunnerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UnityEngine;
+
+using Vaneftec.Game.Common.Controller;
+using Vaneftec.Game.Common.Settings;
+
+// Copyright (c) Florian Steitz
+namespace Vaneftec.Game.Common.Controller {
+
+	/// <summary>
+	/// TODO
+	/// </summary>
+	public class LanePositionController : GameController {
+
+		/// <summary>
+		/// TODO
+		/// </summary>
+		void Update() {
+			Debug.Log("called");
+			Debug.Log(gameSettings.MovementVelocity);
+			transform.Translate(Vector3.forward * gameSettings.MovementVelocity * Time.deltaTime);
+		}
+	}
+}
+
