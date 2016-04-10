27f011bdd6a9f89739b410fe9709efa639f0dd13
Florian Steitz <florian@fsteitz.com>
2016-04-10 16:10:24 +0200

    Added GameSettings. Each game has exactly one instance of it. An instance contains all game settings, regardless of whether a setting is configurable by a user.

---
+++ robot-runner-mobile-game/Assets/Scripts/Common/Settings/GameSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UnityEngine;
+
+// Copyright (c) Florian Steitz
+namespace Vaneftec.Game.Common.Settings {
+
+	/// <summary>
+	/// TODO
+	/// </summary>
+	public class GameSettings : ScriptableObject {
+
+		public float MovementVelocity { get; set; }
+		public float JumpVelocity { get; set; }
+		public float Gravity { get; set; }
+
+		/// <summary>
+		/// TODO
+		/// </summary>
+		public GameSettings() {
+			MovementVelocity = 25.0f;
+			JumpVelocity = 8.0f;
+			Gravity = 9.8F;
+		}
+	}
+}
