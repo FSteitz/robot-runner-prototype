8169b144138de5905acd82cb90fe3a5244018565
Florian Steitz <florian@fsteitz.com>
2017-11-11 19:54:29 +0100

    Relicensed the code to Apache License 2.0

--- robot-runner-mobile-game/Assets/Scripts/Controller/PlayerController.cs
+++ robot-runner-mobile-game/Assets/Scripts/Controller/PlayerController.cs
@@ -3,6 +3,20 @@ using System.Collections;
 
 using Vaneftec.Game.Common.Controller;
 
+/// Copyright 2016 Florian Steitz
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///   http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
 public class PlayerController : GameController {
 
 	private const string COLLECTIBLE_TAG = "Collectible";
@@ -47,12 +61,12 @@ public class PlayerController : GameController {
 					moveRight = true;
 				}
 			}
-			 
+
 			if (Input.GetKey(KeyCode.Space)) {
 				moveDirection.y = jumpVelocity;
 			}
 		}
-    
+
 		if (!isMoving && (moveLeft || moveRight)) {
 			isMoving = true;
 
@@ -68,7 +82,7 @@ public class PlayerController : GameController {
 		}
 
 		if (isMoving) {
-			moveDirection += moveToLane(targetLanePosition, targetDirection);			
+			moveDirection += moveToLane(targetLanePosition, targetDirection);
 		}
 
 		moveDirection.y -= gravity * Time.deltaTime;
